b1e9fb6b384e2a7e65f183fcd38d780d18be8118
HWMooo <58176899+HWMooo@users.noreply.github.com>
2022-03-23 10:48:04 +0000

    db running

---
+++ api/dbConfig/dev_seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO usersAndPosts (user, title, post)
+VALUES
+(
+    'Mooman ryder',
+    'I tried to attack the sea King',
+    'Yesterday i tried to attack the sea King, i am a class C hero i stood no chance and was almost killed.'
+),
+(
+    'Saitama',
+    'I thought the sea King and he was too weak',
+    'Yesterday i went looking for somebody who could give me a serious fight, the sea King was not that person, he did almost kill some guy with a bike.'
+);
